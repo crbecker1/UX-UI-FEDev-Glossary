bda24dbfb6f92fe733ccf12f84702bb0a5b801b2
Chris R. Becker <crbecker1@gmail.com>
2020-02-19 17:20:05 -0800

    fixed filter

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 function myFunction() {
-    var input, filter, cards, cardContainer, h2, title, i;
+    var input, filter, cards, cardContainer, h2, p, title, i;
     input = document.getElementById("myFilter");
     filter = input.value.toUpperCase();
     cardContainer = document.getElementById("myItems");
     cards = cardContainer.getElementsByClassName("card");
     for (i = 0; i < cards.length; i++) {
-        title = cards[i].querySelector(".card-body h5.card-title");
+        title = cards[i].querySelector(".card-body h2.card-title");
         if (title.innerText.toUpperCase().indexOf(filter) > -1) {
             cards[i].style.display = "";
         } else {
